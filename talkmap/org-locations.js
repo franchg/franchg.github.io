add8625aa0e67e8d1d4640cf09f8b22126137656
Gabriele Franch <franch@fbk.eu>
2023-08-24 15:07:01 +0200

    updated talks

--- talkmap/org-locations.js
+++ talkmap/org-locations.js
@@ -1,22 +1,27 @@
 var addressPoints = [
   [
-    "Berkeley CA, USA",
-    37.8708393,
-    -122.2728638
+    "Florence, Italy",
+    43.7698712,
+    11.2555757
   ],
   [
-    "London, UK",
-    51.5073219,
-    -0.1276473
+    "Locarno, Switzerland",
+    46.1695739,
+    8.7954859
   ],
   [
-    "San Francisco, California",
-    37.7792808,
-    -122.4192362
+    "Bologna, Italy",
+    44.4938203,
+    11.3426327
   ],
   [
-    "Los Angeles, CA",
-    34.0543942,
-    -118.2439408
+    "Vienna, Austria",
+    48.2083537,
+    16.3725042
+  ],
+  [
+    "Aquila, Italy",
+    42.3489203,
+    13.3979672
   ]
 ];
